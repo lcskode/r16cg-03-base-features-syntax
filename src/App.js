721d42d0d6677d15de2c043f1a73c6a4ee33843b
Leo <lcskode@gmail.com>
2018-12-09 20:21:36 +0800

    Added code for Person.js and modified App.js to import Person.js on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My First React App</h1>        
+        <h1>My First React App</h1>   
+        <Person />
+        <Person />
+        <Person />     
       </div>
     );
   }
