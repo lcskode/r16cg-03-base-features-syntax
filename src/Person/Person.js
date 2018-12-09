1d2f74a0e64288c7237cb80501f1e4c09a8eb764
Leo <lcskode@gmail.com>
2018-12-09 20:29:52 +0800

    Added props to output different values.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const person = () => {
-  return <p>I'm a person and I'm {Math.floor(Math.random() * 30)}</p>
+const person = (props) => {
+  // Add dynamic content
+  // return <p>I'm a person and I'm {Math.floor(Math.random() * 30)}</p>
+  
+  // Used props instead of dynamic content
+  return <p>I'm {props.name} and I'm {props.age} years old.</p>
 }
 
 export default person;
